218b7983d781f4cf7f0850b267565e7ef176fef1
Patricia <patriciaparreirasainz@gmail.com>
2025-04-13 16:50:26 +0200

    added links to recipes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,28 @@
 import Header from './App/Header';
 import Landing from './App/Landing';
 import Footer from './App/Footer';
-import {Route, Routes} from "react-router-dom";
+import RecipePage from './App/RecipePage/RecipePage';
+// import { useEffect, useState } from 'react';
+import { Route, Routes, matchPath, useLocation } from 'react-router-dom';
+
+import recipes from "../data/recipes";
 
 function App() {
+
+  // const [recipeList, setRecipeList] = useState([]);
+
+  // CREAR RUTAS A CADA RECETA POR SU ID
+  const {pathname} = useLocation()
+  const routeData = matchPath("/recipe/:recipeId", pathname)
+  const recipeIdUrl = routeData ? routeData.params.recipeId : ""
+  const recipeInfo = recipes.find(recipe => recipe.id === recipeIdUrl)
+
   return (
     <>
       <Header></Header>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/recipe/:recipeId" element={<RecipePage recipeInfo={recipeInfo}/>} />
       </Routes>
       <Footer></Footer>
     </>
