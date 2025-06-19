ca61914fb172bd0bf6dd51c242c3095654fbffd7
Patricia Parreira <patriciaparreirasainz@gmail.com>
2025-06-19 14:16:04 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ command }) => ({
   base: command === "serve" ? "/" : "/recipe-book/",
   plugins: [react()],
-  base: "/recipe-book/",
   server: {
     watch: {
       usePolling: true,
